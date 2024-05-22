32ef0a9b47bba4089e2f463e5b88b503c1e0c140
Archibalt <132143853+Pan-Artur@users.noreply.github.com>
2024-05-22 22:53:08 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+console.dir(Math);
+
+const degreeCelsius = 0;
+const division = 9 / 5;
+const multiplication = degreeCelsius * division;
+const add = multiplication + 32;
+const degreeFahrenheit = add;
+console.log(degreeFahrenheit);
+
+const daysOfMay = 31;
+const hoursOfMay = daysOfMay * 24;
+console.log(hoursOfMay);
+const minutesOfDay = (24 * 60);
+const minutesOfMay = hoursOfMay * minutesOfDay;
+console.log(minutesOfMay);
+
+const gamerHealth = 8;
+const gamerEnergy = 6;
+const bossKick = 4;
+const gamerHealthAfterFight = gamerHealth - bossKick;
+console.log(gamerHealthAfterFight);
+const spentEnergy = 4;
+const gamerEnergyAfterFight = gamerEnergy - spentEnergy;
+console.log(gamerEnergyAfterFight);
+
+const purchaseAmount = 1260;
+const sale = 0.1;
+const finalPurchaseAmount = purchaseAmount * sale;
+console.log(finalPurchaseAmount);
+
+const notWholeNumber = 73.6;
+console.log(Math.floor(notWholeNumber));
+
+const notWholeNumberString = "35.4";
+console.log(parseFloat(notWholeNumberString));
+
+const wholeNumberString = "52";
+console.log(parseInt(wholeNumberString));
+
+const number = 25;
+console.log(Math.sqrt(number));
+
+const stringToNumber = "45";
+const numberToString = 89;
+console.log(parseInt(stringToNumber));
+console.log(numberToString.toString());
+
